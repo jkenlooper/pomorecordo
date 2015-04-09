744e806b1361e90be1a6bd81df990875c09838d9
Jake Hickenlooper <jake@weboftomorrow.com>
2015-04-09 07:14:52 -0600

    Update

--- pomorecordo.sh
+++ pomorecordo.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # PomoRecordo.sh
-# Record screen and audio for 25 min pomodoro and then display-message to tmux
-# Includes bonus opening-title
+# Record screen and audio for 25 min pomodoro and display-messages to tmux
+# Includes bonus opening-title creation
 
 TIMER=25;
 OPENINGTITLE=4;
-FRAMERATE=25;
+FRAMERATE=24;
 FPS=12
+REALTIME=0;
+AUDIO=0;
 
-# set this to the minium amount of time between frames that are the same.
+# set this to the minium amount of time (seconds) between frames that are the same.
 IDLE=5;
+MAIN_IDLE=5;
 
 # Load the TASK variable with the todo text
 TASK=`sed -n ''"$1"' L' $HOME/.todo/todo.txt`;
@@ -19,6 +22,7 @@ echo "Starting in:";
 COUNTDOWNTOSCGREENGRAB=10;
 while test $COUNTDOWNTOSCGREENGRAB -gt -1; do
     read -n 1 -p "$COUNTDOWNTOSCGREENGRAB.." -t 1 CANCELSCREENGRAB && break;
+    tmux display-message -c /dev/tty1 "$COUNTDOWNTOSCGREENGRAB"
     COUNTDOWNTOSCGREENGRAB=$(($COUNTDOWNTOSCGREENGRAB-1));
 done;
 
@@ -32,8 +36,6 @@ SGDIR=sg-${START};
 mkdir ${SGDIR};
 cd ${SGDIR};
 
-timer.sh $1 $TIMER &
-TIMER_PID=$!
 END=`date -d "${OPENINGTITLE} minutes" +%s`
 
 FRAME_TIMESTAMP=$(date +%s.%N);
@@ -73,7 +75,8 @@ while test $END -ge $now; do
   read -n 1 -t $timeout CANCELFBGRAB && break;
 
   # Display time left to tmux session occasionaly
-  if test $(($now - $last_message_date)) -gt 1 && test $((($END - $now) % 60)) -eq 0 || test $(($END - $now)) -lt 10; then
+  if test $(($now - $last_message_date)) -gt 1 && test $((($END - $now) % 60)) -eq 0 || \
+      test $(($END - $now)) -lt 10 && test $(($END - $now)) -gt 5; then
       tmux display-message -c /dev/tty1 "$(($END - $now)) seconds left"
       # Update last_message_date to avoid displaying multiple messages.
       last_message_date=$now
@@ -81,12 +84,12 @@ while test $END -ge $now; do
 done;
 if test $CANCELFBGRAB; then
     echo screen grab canceled.
-    kill $TIMER_PID;
     #TODO: cleanup any frame-*.png created?
 else
 
 # copy the last frame a bunch so the opening title will show longer.
 # 50 being FRAMERATE * 2 for about 2 seconds
+# TODO: could be done a different way by using ffmpeg -concat
 for i in {1..50}; do
     cp frame-${SG}.png frame-$(($SG + 1)).png
     SG=$(($SG+1));
@@ -96,16 +99,76 @@ done;
 tmux display-message -c /dev/tty1 "${TASK}"
 
 # Start audio recording and put it in the background
+if test $AUDIO -eq 1; then
 arecord -d $(($TIMER * 60)) -f dat out.wav &
 AUDIO_PID=$!
+fi
 
+timer.sh $1 $TIMER &
+TIMER_PID=$!
 STOP=`date +%s`;
 
 # start recording the main session
-ffmpeg -t $((60 * $TIMER)) -f fbdev -r $FPS -i /dev/fb0 -b:v 10000000 -vcodec mpeg4 out.mov;
-kill $AUDIO_PID
+if test $REALTIME -eq 1; then
+ffmpeg -t $((60 * $TIMER)) -f fbdev -r $FPS -i /dev/fb0 -b:v 10000000 -vcodec mpeg4 main.mov;
+else
+FRAME_TIMESTAMP=$(date +%s.%N);
+END=`date -d "$TIMER minutes" +%s`
 
-tmux display-message -c /dev/tty1 "Complete..."
+frameinterval=$(echo "scale=6; 1/12;" | bc);
+#MainScreenGrab ... not the Mono Sodium Glutamate...
+MSG=1
+fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null &
+now=$(date +%s)
+last_frame_date=$now
+last_message_date=$now
+while test $END -ge $now; do
+  now=$(date +%s)
+  MSG=$(($MSG+1));
+
+  FRAME_TIMESTAMP_NOW=$(date +%s.%N);
+  DELAY=$(echo "scale=3; (($FRAME_TIMESTAMP_NOW - $FRAME_TIMESTAMP)/1) - $frameinterval;" | bc);
+  FRAME_TIMESTAMP=$FRAME_TIMESTAMP_NOW
+  previous_frame=m-$(($MSG - 1)).png
+  frame=m-${MSG}.png
+
+  fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null
+
+  # Throw out this frame if it's the same as previous ( and adjust $findex )
+  if test $(($now - $last_frame_date)) -lt $MAIN_IDLE && test $(compare -metric AE ${previous_frame} ${frame} diff.png 2>&1) -eq 0; then
+      rm ${frame};
+      MSG=$(($MSG - 1));
+  else
+      #mogrify -scale 1440x1080 +repage ${frame} &
+      # It's been more then $MAIN_IDLE seconds since the last frame or it was different.
+      # The last_frame_date marks the time each frame has been added.
+      last_frame_date=$now
+  fi
+
+  # Allow for pressing a key to stop taking frames.
+  # This is simply done to have something to trigger a stop.
+  timeout=$(echo "$frameinterval - $DELAY" | bc);
+  read -n 1 -t $timeout CANCELFBGRAB && break;
+
+done;
+
+tmux display-message -c /dev/tty1 "Complete... Take 5. That's a wrap."
+
+screensize=$(identify -format %wx%h m-1.png)
+
+# Create a time lapse of the m-*.png with about 24 fps
+ffmpeg -framerate 24 -i m-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r 24 main.mov && \
+    rm m-*.png;
+
+rm m-*.png;
+rm diff.png;
+fi
+
+
+if test $AUDIO -eq 1; then
+kill $AUDIO_PID
+fi
+
 
 echo "Converting ${SG} frames for opening title";
 
@@ -138,8 +201,9 @@ while test $frame_index -lt $SG; do
 
     # Make it big ( use -scale as it's better for keeping the pixels crisp )
     # Also center it within the frame.
-    mogrify -scale 1024x768 +repage ${frame};
-    convert -size 1024x768 canvas:black \
+    # 1440x1080
+    mogrify -scale ${screensize} +repage ${frame};
+    convert -size ${screensize} canvas:black \
         -gravity center $frame -compose Over -composite \
         f-${GS}.png;
     rm $frame;
@@ -150,11 +214,16 @@ while test $frame_index -lt $SG; do
 done;
 
 # Note that this is effectivley at a fast speed which should result in a short 5~ second opening title mov.
-ffmpeg -framerate 25 -i f-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r 25 opening-title.mov;
+# TODO: set the input framerate to be adjusted to drop or add frames to keep a set 5 second opening title.
+inputframerate=$((${SG}/5));
+ffmpeg -framerate $inputframerate -i f-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r 24 opening-title.mov && \
+    rm f-*.png;
 
-rm f-*.png;
+# Combine the two vidoes using ffmpeg.
+ffmpeg -f concat -i <(for f in $PWD/opening-title.mov $PWD/main.mov; do echo "file '$f'"; done) -c copy out.mov && \
+    rm opening-title.mov main.mov;
 
-# TODO: combine the two vidoes and audio using ffmpeg.
+
 
 fi
 fi
