46578f67cac8042bf82d874566d2be501537980c
Jake Hickenlooper <jake@weboftomorrow.com>
2021-08-02 08:18:10 -0600

    Fix and clean up WIP
    Switch screen grab and such to use x11.  Lots of other stuff.

--- pomorecordo.sh
+++ pomorecordo.sh
@@ -6,114 +6,127 @@
 #
 # First arg is the todo line number, Second is optional directory to use.  Will continue if there is an existing vid.
 
-TIMER=25;
-OPENINGTITLE=4;
+TIMER=1;
+OPENINGTITLE=1;
 FRAMERATE=12;
 FPS=12
 
-REALTIME=1;
+REALTIME=0;
 TIMELAPSE=1;
-AUDIO=1;
+AUDIO=0;
 
 CONTINUATION=0;
 # If second arg then this is a continuation if there is an existing vid, otherwise use the directory if it exists
 SGDIR=$2;
 if test "$2" -a -d "$2" -a -f "$2/opening-and-timelapse.mov"; then
-    CONTINUATION=1;
-    echo "Continuing recording in: $2";
+  CONTINUATION=1;
+  echo "Continuing recording in: $2";
 fi
 
 # set this to the minium amount of time (seconds) between frames that are the same.
 IDLE=5;
 MAIN_IDLE=1;
 
+BACKGROUND_COLOR="#262626"
+
 # Load the TASK variable with the todo text
-TASK=`sed -n ''"$1"' L' $HOME/.todo/todo.txt`;
-echo "Starting timer for task: ${TASK}"
+#TASK=`sed -n ''"$1"' L' $HOME/.todo/todo.txt`;
+#echo "Starting timer for task: ${TASK}"
 echo "Starting in:";
 COUNTDOWNTOSCGREENGRAB=10;
 while test $COUNTDOWNTOSCGREENGRAB -gt -1; do
-    read -n 1 -p "$COUNTDOWNTOSCGREENGRAB.." -t 1 CANCELSCREENGRAB && break;
-    tmux display-message -c /dev/tty1 "$COUNTDOWNTOSCGREENGRAB"
-    COUNTDOWNTOSCGREENGRAB=$(($COUNTDOWNTOSCGREENGRAB-1));
+  read -n 1 -p "$COUNTDOWNTOSCGREENGRAB.." -t 1 CANCELSCREENGRAB && break;
+  tmux display-message "$COUNTDOWNTOSCGREENGRAB"
+  COUNTDOWNTOSCGREENGRAB=$(($COUNTDOWNTOSCGREENGRAB-1));
 done;
 
-(
 if test $CANCELSCREENGRAB; then
-echo 'canceled screen grab';
-else
+  echo 'canceled screen grab';
+  exit 0
+fi
 
 # Make a new folder in the cwd if no SGDIR has been set
 START=`date +%s`;
 if test ! -n "$SGDIR"; then
-SGDIR=sg-${START};
+  SGDIR=sg-${START};
 fi
 mkdir -p ${SGDIR};
 
 echo "Recording in ${SGDIR}";
 cd ${SGDIR};
 
+
+import -window root screensize-test.png
+screensize=$(identify -format %wx%h screensize-test.png)
+rm screensize-test.png
+
 if test $CONTINUATION -eq 0; then
 
-END=`date -d "${OPENINGTITLE} minutes" +%s`
+  END=`date -d "${OPENINGTITLE} minutes" +%s`
 
-FRAME_TIMESTAMP=$(date +%s.%N);
+  FRAME_TIMESTAMP=$(date +%s.%N);
 
-frameinterval=$(echo "scale=6; 1/12;" | bc);
-# Record the opening title
-SG=1
-fbgrab -d /dev/fb0 frame-${SG}.png 2> /dev/null &
-now=$(date +%s)
-last_frame_date=$now
-last_message_date=$now
-while test $END -ge $now; do
+  frameinterval=$(echo "scale=6; 1/12;" | bc);
+  # Record the opening title
+  SG=1
+  #fbgrab -d /dev/fb0 frame-${SG}.png 2> /dev/null &
+  import -window root frame-${SG}.png
   now=$(date +%s)
-  SG=$(($SG+1));
+  last_frame_date=$now
+  last_message_date=$now
+  while test $END -ge $now; do
+    now=$(date +%s)
+    SG=$(($SG+1));
 
-  FRAME_TIMESTAMP_NOW=$(date +%s.%N);
-  DELAY=$(echo "scale=3; (($FRAME_TIMESTAMP_NOW - $FRAME_TIMESTAMP)/1) - $frameinterval;" | bc);
-  FRAME_TIMESTAMP=$FRAME_TIMESTAMP_NOW
-  previous_frame=frame-$(($SG - 1)).png
-  frame=frame-${SG}.png
+    FRAME_TIMESTAMP_NOW=$(date +%s.%N);
+    DELAY=$(echo "scale=3; (($FRAME_TIMESTAMP_NOW - $FRAME_TIMESTAMP)/1) - $frameinterval;" | bc);
+    FRAME_TIMESTAMP=$FRAME_TIMESTAMP_NOW
+    previous_frame=frame-$(($SG - 1)).png
+    frame=frame-${SG}.png
 
-  fbgrab -d /dev/fb0 frame-${SG}.png 2> /dev/null
+    #fbgrab -d /dev/fb0 frame-${SG}.png 2> /dev/null
+    import -window root frame-${SG}.png
 
-  # Throw out this frame if it's the same as previous ( and adjust $findex )
-  if test $(($now - $last_frame_date)) -lt $IDLE && test $(compare -metric AE ${previous_frame} ${frame} diff.png 2>&1) -eq 0; then
-      rm ${frame};
-      SG=$(($SG - 1));
-  else
-      # It's been more then $IDLE seconds since the last frame or it was different.
-      # The last_frame_date marks the time each frame has been added.
-      last_frame_date=$now
-  fi
+    # Throw out this frame if it's the same as previous ( and adjust $findex )
+    if test $(($now - $last_frame_date)) -lt $IDLE && test $(compare -metric AE ${previous_frame} ${frame} diff.png 2>&1) -eq 0; then
+        rm ${frame};
+        SG=$(($SG - 1));
+    else
+        # It's been more then $IDLE seconds since the last frame or it was different.
+        # The last_frame_date marks the time each frame has been added.
+        last_frame_date=$now
+    fi
 
-  # Allow for pressing a key to stop taking frames.
-  # This is simply done to have something to trigger a stop.
-  timeout=$(echo "$frameinterval - $DELAY" | bc);
-  read -n 1 -t $timeout CANCELFBGRAB && break;
+    # Allow for pressing a key to stop taking frames.
+    # This is simply done to have something to trigger a stop.
+    # timeout should only be an integer and not be less than 0.
+    timeout=$(echo "$frameinterval - $DELAY" | bc | xargs printf "%.*f" 0 );
+    timeout=$(test $timeout -lt 0 && echo "0" || echo $timeout)
 
-  # Display time left to tmux session occasionaly
-  if test $(($now - $last_message_date)) -gt 1 && test $((($END - $now) % 60)) -eq 0 || \
-      test $(($END - $now)) -lt 10 && test $(($END - $now)) -gt 5; then
-      tmux display-message -c /dev/tty1 "$(($END - $now)) seconds left"
-      # Update last_message_date to avoid displaying multiple messages.
-      last_message_date=$now
-  fi
-done;
+    read -n 1 -t $timeout CANCELFBGRAB && break;
+
+    # Display time left to tmux session occasionaly
+    if test $(($now - $last_message_date)) -gt 1 && test $((($END - $now) % 60)) -eq 0 || \
+        test $(($END - $now)) -lt 10 && test $(($END - $now)) -gt 5; then
+        tmux display-message "$(($END - $now)) seconds left"
+        # Update last_message_date to avoid displaying multiple messages.
+        last_message_date=$now
+    fi
+  done;
 fi
 if test $CANCELFBGRAB; then
     echo screen grab canceled.
     #TODO: cleanup any frame-*.png created?
-else
+    exit 0
+fi
 
 # signal that recording has now begun.
-tmux display-message -c /dev/tty1 "${TASK}"
+#tmux display-message "${TASK}"
 
 # Start audio recording and put it in the background
 if test $AUDIO -eq 1; then
-arecord -d $(($TIMER * 60)) -f dat out.wav &
-AUDIO_PID=$!
+  arecord -d $(($TIMER * 60)) -f dat out.wav &
+  AUDIO_PID=$!
 fi
 
 timer.sh $1 $TIMER &
@@ -123,176 +136,178 @@ STOP=`date +%s`;
 END=`date -d "$TIMER minutes" +%s`
 # start recording the main session
 if test $REALTIME -eq 1; then
-REALTIME_I=''
-while test -f realtime${REALTIME_I}.mov; do
-    REALTIME_I=$((${REALTIME_I}+1));
-done;
+  REALTIME_I=''
+  while test -f realtime${REALTIME_I}.mp4; do
+      REALTIME_I=$((${REALTIME_I}+1));
+  done;
 
-ffmpeg -t $((60 * $TIMER)) -f fbdev -r $FPS -i /dev/fb0 -b:v 10000000 -vcodec mpeg4 realtime${REALTIME_I}.mov &
+  ffmpeg -t $((60 * $TIMER)) -f x11grab -framerate $FPS -i :0.0 -video_size $screensize realtime${REALTIME_I}.mp4 &
 fi
+
 if test $TIMELAPSE -eq 1; then
-FRAME_TIMESTAMP=$(date +%s.%N);
+  FRAME_TIMESTAMP=$(date +%s.%N);
 
-frameinterval=$(echo "scale=6; 1/12;" | bc);
-#MainScreenGrab ... not the Mono Sodium Glutamate...
-MSG=1
-fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null &
-now=$(date +%s)
-show_large_diff=$now
-idle=$MAIN_IDLE
-last_frame_date=$now
-last_message_date=$now
-while test $END -ge $now; do
+  frameinterval=$(echo "scale=6; 1/12;" | bc);
+  #MainScreenGrab ... not the Mono Sodium Glutamate...
+  MSG=1
+  #fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null &
+  import -window root m-${MSG}.png
   now=$(date +%s)
-  MSG=$(($MSG+1));
+  show_large_diff=$now
+  idle=$MAIN_IDLE
+  last_frame_date=$now
+  last_message_date=$now
+  while test $END -ge $now; do
+    now=$(date +%s)
+    MSG=$(($MSG+1));
 
-  FRAME_TIMESTAMP_NOW=$(date +%s.%N);
-  DELAY=$(echo "scale=3; (($FRAME_TIMESTAMP_NOW - $FRAME_TIMESTAMP)/1) - $frameinterval;" | bc);
-  FRAME_TIMESTAMP=$FRAME_TIMESTAMP_NOW
-  previous_frame=m-$(($MSG - 1)).png
-  frame=m-${MSG}.png
+    FRAME_TIMESTAMP_NOW=$(date +%s.%N);
+    DELAY=$(echo "scale=3; (($FRAME_TIMESTAMP_NOW - $FRAME_TIMESTAMP)/1) - $frameinterval;" | bc);
+    FRAME_TIMESTAMP=$FRAME_TIMESTAMP_NOW
+    previous_frame=m-$(($MSG - 1)).png
+    frame=m-${MSG}.png
 
-  fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null
+    #fbgrab -d /dev/fb0 m-${MSG}.png 2> /dev/null
+    import -window root m-${MSG}.png
 
-  # Throw out this frame if it's the same as previous ( and adjust $findex )
-  if test $(($now - $last_frame_date)) -lt $MAIN_IDLE && \
-      test $(compare -metric AE ${previous_frame} ${frame} -compose Src diff.png 2>&1 | tee diff) -eq 0; then
-      # Only remove the frame if the skipframe flag is set.
-      if test $show_large_diff -lt $now; then
-          rm ${frame};
-          MSG=$(($MSG - 1));
-      fi
-  else
-      #mogrify -scale 1440x1080 +repage ${frame} &
-      # It's been more then $MAIN_IDLE seconds since the last frame or it was different.
-      # The last_frame_date marks the time each frame has been added.
-      last_frame_date=$now
+    # Throw out this frame if it's the same as previous ( and adjust $findex )
+    if test $(($now - $last_frame_date)) -lt $MAIN_IDLE && \
+        test $(compare -metric AE ${previous_frame} ${frame} -compose Src diff.png 2>&1 | tee diff) -eq 0; then
+        # Only remove the frame if the skipframe flag is set.
+        if test $show_large_diff -lt $now; then
+            rm ${frame};
+            MSG=$(($MSG - 1));
+        fi
+    else
+        #mogrify -scale 1440x1080 +repage ${frame} &
+        # It's been more then $MAIN_IDLE seconds since the last frame or it was different.
+        # The last_frame_date marks the time each frame has been added.
+        last_frame_date=$now
 
-      #last frame was different
-      if test -f diff.png; then
-          # check the diff.png for the size of the difference and set the skipframe flag
-          if test $(echo "$(convert diff.png -trim -format '%w * %h' info:)" | bc) -gt 248000; then
-              show_large_diff=$(($now+6))
-              rm diff.png;
-          fi
-      fi
-  fi
+        #last frame was different
+        if test -f diff.png; then
+            # check the diff.png for the size of the difference and set the skipframe flag
+            if test $(echo "$(convert diff.png -trim -format '%w * %h' info:)" | bc) -gt 248000; then
+                show_large_diff=$(($now+6))
+                rm diff.png;
+            fi
+        fi
+    fi
 
-  # Allow for pressing a key to stop taking frames.
-  # This is simply done to have something to trigger a stop.
-  timeout=$(echo "$frameinterval - $DELAY" | bc);
-  read -n 1 -t $timeout CANCELFBGRAB && break;
+    # Allow for pressing a key to stop taking frames.
+    # This is simply done to have something to trigger a stop.
+    # timeout should only be an integer and not be less than 0.
+    timeout=$(echo "$frameinterval - $DELAY" | bc | xargs printf "%.*f" 0 );
+    timeout=$(test $timeout -lt 0 && echo "0" || echo $timeout)
+    #echo "timeout is $timeout"
+    read -n 1 -t $timeout CANCELFBGRAB && break;
 
-done;
+  done;
 
-DONEMSG=5;
-while test $DONEMSG -gt -1; do
-    tmux display-message -c /dev/tty1 "Complete... Take 5. That's a wrap."
-    sleep 1;
-    DONEMSG=$(($DONEMSG-1));
-done;
+  # Stop the timer.sh in case the screen grab was cancelled.
+  kill $TIMER_PID 2> /dev/null || echo 'timer finished'
 
-screensize=$(identify -format %wx%h m-1.png)
+  DONEMSG=5;
+  while test $DONEMSG -gt -1; do
+      # so cheesy...
+      tmux display-message "Complete... Take 5. That's a wrap."
+      sleep 1;
+      DONEMSG=$(($DONEMSG-1));
+  done;
 
-# Create a time lapse of the m-*.png with about 12 fps
-ffmpeg -framerate ${FRAMERATE} -i m-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r ${FRAMERATE} timelapse.mov && \
-    rm m-*.png;
+  # Create a time lapse of the m-*.png with about 12 fps
+  ffmpeg -framerate ${FRAMERATE} -i m-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r ${FRAMERATE} timelapse.mov
 
-rm m-*.png;
-rm diff.png;
+  rm m-*.png;
+  rm -f diff.png;
 fi
 
 # Stall while the REALTIME recording might still be going
 now=$(date +%s)
 if test $END -ge $now; then
-echo "waiting every 5 seconds since timer not complete."
+  echo "waiting every 5 seconds since timer not complete."
 fi
 while test $END -ge $now; do
-  read -n 1 -t 5 -p "." && break
-  now=$(date +%s)
+read -n 1 -t 5 -p "." && break
+now=$(date +%s)
 done;
 
 if test $AUDIO -eq 1; then
-kill $AUDIO_PID
+  kill $AUDIO_PID
 fi
 
 if test $CONTINUATION -eq 0; then
 
-echo "Converting ${SG} frames for opening title";
+  echo "Converting ${SG} frames for opening title";
 
 
-frame_index=0
-GS=$SG
-last_dim_w=1
-last_dim_h=1
-while test $frame_index -lt $SG; do
-    frame_index=$((${frame_index} + 1))
-    frame=frame-${frame_index}.png
-    echo "building opening title ${frame} of ${SG}";
+  frame_index=0
+  GS=$SG
+  last_dim_w=1
+  last_dim_h=1
+  while test $frame_index -lt $SG; do
+      frame_index=$((${frame_index} + 1))
+      frame=frame-${frame_index}.png
+      echo "building opening title ${frame} of ${SG}";
 
-    # Crop out the left and bottom edges and then trim what's left
-    mogrify -crop +0-80 +repage -crop +30+0 +repage \
-        -trim +repage -bordercolor black -border 20x20 ${frame};
+      # Crop out the left and bottom edges and then trim what's left
+      mogrify -crop +0-80 +repage -crop +30+0 +repage \
+          -trim +repage -bordercolor $BACKGROUND_COLOR -border 20x20 ${frame};
 
-    frame_w=$(identify -format "%w" ${frame});
-    frame_h=$(identify -format "%h" ${frame});
-    if test $frame_w -gt $last_dim_w; then
-        last_dim_w=$frame_w
-    fi
-    if test $frame_h -gt $last_dim_h; then
-        last_dim_h=$frame_h
-    fi
+      frame_w=$(identify -format "%w" ${frame});
+      frame_h=$(identify -format "%h" ${frame});
+      if test $frame_w -gt $last_dim_w; then
+          last_dim_w=$frame_w
+      fi
+      if test $frame_h -gt $last_dim_h; then
+          last_dim_h=$frame_h
+      fi
 
-    # Add black bg with cropped frame positioned on top
-    # Using default gravity of top left to avoid jumpiness ( as long as the top and left edges remain the same )
-    mogrify -background black -extent ${last_dim_w}x${last_dim_h} +repage ${frame};
+      # Add background color with cropped frame positioned on top
+      # Using default gravity of top left to avoid jumpiness ( as long as the top and left edges remain the same )
+      mogrify -background $BACKGROUND_COLOR -extent ${last_dim_w}x${last_dim_h} +repage ${frame};
 
-    # Make it big ( use -scale as it's better for keeping the pixels crisp )
-    # Also center it within the frame.
-    # 1440x1080
-    mogrify -scale ${screensize} +repage ${frame};
-    convert -size ${screensize} canvas:black \
-        -gravity center $frame -compose Over -composite \
-        f-${GS}.png;
-    rm $frame;
+      # Make it big ( use -scale as it's better for keeping the pixels crisp )
+      # Also center it within the frame.
+      # 1440x1080
+      mogrify -scale ${screensize} +repage ${frame};
+      convert -size ${screensize} canvas:$BACKGROUND_COLOR \
+          -gravity center $frame -compose Over -composite \
+          f-${GS}.png;
+      rm $frame;
 
-    # And this is how it is reversed
-    GS=$((${GS} - 1))
+      # And this is how it is reversed
+      GS=$((${GS} - 1))
 
-done;
+  done;
 
-cp f-1.png opening-title.png;
+  cp f-1.png opening-title.png;
 
-# Now do an unreverse since the f-*.png have been properly cropped
-# f-*.png -> g-*.png but in reversed index
-frame_index=$SG;
-newframe_index=1;
-while test $frame_index -gt 0; do
-    mv f-${frame_index}.png g-${newframe_index}.png;
-    newframe_index=$((${newframe_index} + 1))
-    frame_index=$((${frame_index} - 1))
-done;
+  # Now do an unreverse since the f-*.png have been properly cropped
+  # f-*.png -> g-*.png but in reversed index
+  frame_index=$SG;
+  newframe_index=1;
+  while test $frame_index -gt 0; do
+      mv f-${frame_index}.png g-${newframe_index}.png;
+      newframe_index=$((${newframe_index} + 1))
+      frame_index=$((${frame_index} - 1))
+  done;
 
-# Note that this is effectivley at a fast speed which should result in a short 5~ second opening title mov.
-inputframerate=$((${SG}/5));
-ffmpeg -framerate $inputframerate -i g-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r ${FRAMERATE} opening-title.mov && \
-    rm g-*.png;
+  # Note that this is effectivley at a fast speed which should result in a short 5~ second opening title mov.
+  inputframerate=$((${SG}/5));
+  ffmpeg -framerate $inputframerate -i g-%d.png -c:v libx264 -pix_fmt yuv420p -b:v 10000000 -vcodec mpeg4 -r ${FRAMERATE} opening-title.mov && \
+      rm g-*.png;
 
 
-# Combine the two vidoes using ffmpeg.
-ffmpeg -f concat -i <(for f in $PWD/opening-title.mov $PWD/timelapse.mov; do echo "file '$f'"; done) -c copy opening-and-timelapse.mov && \
-    rm opening-title.mov timelapse.mov;
+  # Combine the two vidoes using ffmpeg.
+  ffmpeg -f concat -safe 0 -i <(for f in $PWD/opening-title.mov $PWD/timelapse.mov; do echo "file '$f'"; done) -c copy opening-and-timelapse.mov && \
+      rm opening-title.mov timelapse.mov;
 
 else
 
-# Combine this video on the last one since this is a CONTINUATION
-mv opening-and-timelapse.mov previous.mov;
-ffmpeg -f concat -i <(for f in $PWD/previous.mov $PWD/timelapse.mov; do echo "file '$f'"; done) -c copy opening-and-timelapse.mov && \
-    rm previous.mov timelapse.mov;
+  # Combine this video on the last one since this is a CONTINUATION
+  mv opening-and-timelapse.mov previous.mov;
+  ffmpeg -f concat -safe 0 -i <(for f in $PWD/previous.mov $PWD/timelapse.mov; do echo "file '$f'"; done) -c copy opening-and-timelapse.mov && \
+      rm previous.mov timelapse.mov;
 
 fi
-
-
-fi
-fi
-)
